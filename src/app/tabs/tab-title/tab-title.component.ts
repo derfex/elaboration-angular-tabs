34b51faa0e5abd7e0ac9de5efd5608ca2715e8db
derfex <derfex@yandex.ru>
2020-06-17 21:49:27 +0300

    Style (tabs): use classes from `styles.sass`

--- src/app/tabs/tab-title/tab-title.component.ts
+++ src/app/tabs/tab-title/tab-title.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  ElementRef,
+  Renderer2,
 } from '@angular/core';
 
 
@@ -9,4 +11,12 @@ import {
   styleUrls: ['./tab-title.component.sass'],
 })
 export class TabTitleComponent {
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2,
+  ) {
+    renderer.addClass(elementRef.nativeElement, cssClassTitle);
+  }
 }
+
+const cssClassTitle: string = 'tabs__title';
