3aaeb701f4f5fd69e833bfeddf7bfc909f29f392
derfex <derfex@yandex.ru>
2020-06-17 19:08:05 +0300

    Feature (tabs): activate a tab by click, output `tab-content` for active tabs only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
 } from '@angular/core';
+import {
+  Subscription,
+} from 'rxjs';
+
+import { TabComponent } from './tabs/tab/tab.component';
 
 
 @Component({
@@ -10,8 +21,44 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
+  public activeTab: TabComponent;
   public tabs: number[] = [1, 2];
+
+  @HostListener('click', ['$event.target'])
+  private onMouseClick(targetNativeElement: HTMLElement): void {
+    const tabTitleElement: HTMLElement = targetNativeElement.closest('tab-title');
+    if (!tabTitleElement) {
+      return;
+    }
+    const tabNumber: number = +tabTitleElement.firstElementChild.innerHTML;
+    this.activate.emit(tabNumber);
+  }
+
+  @ViewChildren(TabComponent)
+  private tabComponents: QueryList<TabComponent>;
+
+  private activate: EventEmitter<number> = new EventEmitter<number>();
+  private subscriptions: Subscription = new Subscription();
+
+
+  public ngOnInit(): void {
+    this.subscriptions.add(
+      this.activate.subscribe((tabNumber: number) => {
+        const tab: TabComponent = this.getTabComponentByIndex(tabNumber - 1);
+        if (this.activeTab) {
+          this.activeTab.active = false;
+        }
+        tab.active = true;
+        this.activeTab = tab;
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
 
   public dec(): void {
     this.tabs = this.tabs.slice(0, -1);
@@ -19,5 +66,9 @@ export class AppComponent {
 
   public inc(): void {
     this.tabs = [...this.tabs, (this.tabs.length + 1)];
+  }
+
+  private getTabComponentByIndex(index: number): TabComponent {
+    return this.tabComponents.toArray()[index];
   }
 }
--- src/app/tabs/tab/tab.component.ts
+++ src/app/tabs/tab/tab.component.ts
@@ -1,5 +1,8 @@
 import {
   Component,
+  ElementRef,
+  Input,
+  Renderer2,
 } from '@angular/core';
 
 
@@ -9,4 +12,31 @@ import {
   styleUrls: ['./tab.component.sass'],
 })
 export class TabComponent {
+  @Input()
+  public get active(): boolean {
+    return this._active;
+  }
+
+  public set active(active: boolean) {
+    this.renderActive(active);
+    this._active = active;
+  }
+
+  private _active: boolean = false;
+
+
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2,
+  ) {
+  }
+
+
+  private renderActive(active: boolean): void {
+    const method: string = active ? 'addClass' : 'removeClass';
+    const tabTitle: HTMLElement = this.elementRef.nativeElement.firstElementChild;
+    this.renderer[method](tabTitle, cssClassTitleActive);
+  }
 }
+
+const cssClassTitleActive: string = 'tabs__title--active';
